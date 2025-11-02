8814a9637736878698a304ff3a506bd88dd36046
KimJunsu <12jangdokg55@gmail.com>
2025-11-02 21:00:28 +0900

    기본 버전 세팅

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.moodiary"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.moodiary"
         minSdk = 25
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
