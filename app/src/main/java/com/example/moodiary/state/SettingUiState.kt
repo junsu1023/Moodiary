5ebce1b0986a2a92979748a169684fe205819546
junsu1023 <jangdokg12@naver.com>
2025-11-25 21:09:37 +0900

    비밀번호 변경 다이얼로그 UI 구현

--- app/src/main/java/com/example/moodiary/state/SettingUiState.kt
+++ app/src/main/java/com/example/moodiary/state/SettingUiState.kt
@@ -1,8 +1,20 @@
-
 package com.example.moodiary.state
 
 data class SettingUiState(
     val notificationsEnabled: Boolean = true,
     val darkModeEnabled: Boolean = false,
-    val isShowLogoutDialog: Boolean = false
+    val isShowLogoutDialog: Boolean = false,
+    val dialogKind: DialogKind = DialogKind.NoShow
 )
+
+data class ChangePasswordState(
+    val curPassword: String = "",
+    val newPassword: String = "",
+    val confirmNewPassword: String = ""
+)
+
+sealed class DialogKind {
+    object NoShow: DialogKind()
+    object Logout: DialogKind()
+    object ChangePassword: DialogKind()
+}
